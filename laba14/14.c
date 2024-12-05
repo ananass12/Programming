a7a704ea261e56b8555c4949e11bcdd717d8e3ca
Anastasia <116446113+ananass12@users.noreply.github.com>
2024-12-05 08:05:47 +0700

    Update 14.c

--- laba14/14.c
+++ laba14/14.c
@@ -1,42 +1,35 @@
-//Заменить начальные буквы всех слов на прописные буквы и записать результирующий текст в новый файл.
-
+//заменить начальные буквы всех слов на прописные буквы и записать результирующий текст в новый файл.
 #include <stdio.h>
 #include <ctype.h>
 
 int main() {
-    // Определение имени входного и выходного файлов
-    char inputFileName[] = "sample2.txt";
-    char outputFileName[] = "sample2_out.txt";
+    char inputFileName[] = "sample2.txt";     
+    char outputFileName[] = "sample2_out.txt";     
     
-    // Открытие входного и выходного файлов
     FILE *inputFile = fopen(inputFileName, "r");
     FILE *outputFile = fopen(outputFileName, "w");
 
-    // Проверка успешности открытия файлов
     if (inputFile == NULL || outputFile == NULL) {
         printf("Ошибка открытия файла\n");
         return 1;
     }
 
-    int capitalize = 1; // Флаг, указывающий на необходимость написания следующей буквы заглавной
+    int capitalize = 1;     
     int c;
-
-    // Построчное чтение из входного файла
-    while ((c = fgetc(inputFile)) != EOF) {
+  
+    while ((c = fgetc(inputFile)) != EOF) {         
         if (capitalize && isalpha(c)) {
-            fputc(toupper(c), outputFile); // Преобразование первой буквы слова в заглавную
+            fputc(toupper(c), outputFile);       //первая буква заглавная
             capitalize = 0;
         } else {
-            fputc(tolower(c), outputFile); // Преобразование остальных букв слова в строчные
+            fputc(tolower(c), outputFile);          //остальные букв слова в строчные
         }
 
-        // Если текущий символ - пробел, новая строка или табуляция, устанавливаем флаг capitalize
-        if (c == ' ' || c == '\n' || c == '\t') {
+        if (c == ' ' || c == '\n' || c == '\t') {     //если текущий символ-пробел, новая строка, табуляция
             capitalize = 1;
         }
     }
 
-    // Закрытие файлов
     fclose(inputFile);
     fclose(outputFile);
 
