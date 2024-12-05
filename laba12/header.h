46598bf74967d82dce9b260a53202db58cea4a39
Anastasia <116446113+ananass12@users.noreply.github.com>
2024-12-05 09:08:06 +0700

    Update header.h

--- laba12/header.h
+++ laba12/header.h
@@ -1,17 +1,14 @@
 #ifndef HEADER_H
 #define HEADER_H
-
 #include <stdio.h>
 
-// Структура для представления прямой y = a * x + b
-struct Line {
-    float a; // Коэффициент a
-    float b; // Коэффициент b
+struct Line {   //структура для представления прямой y = a * x + b
+    float a; 
+    float b; 
 };
 
-// Функции для подсчета количества прямых, параллельных осям координат
 int countLinesOX(struct Line lines[], int size);
 int countLinesOY(struct Line lines[], int size);
 
-#endif // HEADER_H
+#endif 
 
